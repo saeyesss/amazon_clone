32445544b0471ea94811c958f150dff536cc7179
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-09-04 01:00:35 +0530

    theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,9 +14,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("hello"),
+        ),
+        body: const Center(
+          child: Text('Flutter Demo Home Page'),
+        ),
+      ),
     );
   }
 }
